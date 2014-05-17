e14a966de881b6939b126027b16641f929dd5b49
Dean Richer <dpbricher@gmail.com>
2014-05-17 13:16:59 +0100

    Flex config parser now reads the 'append' attribute of the 'source-path' element

--- src/flex_config.py
+++ src/flex_config.py
@@ -1,6 +1,11 @@
 import os.path
 import xml.dom.minidom as Xml
 
+#
+# TODO:
+# Check if config xml is allowed multiples of source-path, include-libraries, etc
+# Check what all valid node types are for children of source path, include-libraries, etc
+#
 class FlexConfigParser:
     def __init__(self):
         self.cConfigXml     = None
@@ -12,6 +17,8 @@ class FlexConfigParser:
 
         self.sConfigPath    = None
         self.sConfigDir     = None
+
+        self.bAppendSrcPath = None
 
     def readConfig(self, sConfigPath):
         self.sConfigPath    = sConfigPath
@@ -26,28 +33,41 @@ class FlexConfigParser:
 
         self.sFlashVersion  = ""
 
+        self.bAppendSrcPath = False
+
         # get flash version
-        cVersionNodes   = self.cConfigXml.getElementsByTagName("target-player")
+        cVersionNodes       = self.cConfigXml.getElementsByTagName("target-player")
 
         if cVersionNodes != None:
             self.sFlashVersion  = cVersionNodes[0].firstChild.data
 
         # find additional src paths
-        cSourcePaths    = self.cConfigXml.getElementsByTagName("source-path")
+        cSourcePaths        = self.cConfigXml.getElementsByTagName("source-path")
 
         if cSourcePaths != None:
-            cSourcePaths    = cSourcePaths[0].getElementsByTagName("path-element")
+            # only take note of the first source path element since I don't think it accepts multiple, but should check
+            cSourcePaths    = cSourcePaths[0]
 
-            for cPath in cSourcePaths:
+            # get append setting
+            sAppend         = cSourcePaths.getAttribute("append")
+
+            if sAppend == "true":
+                self.bAppendSrcPath = True
+            
+            # extract all paths
+            cPathsList      = cSourcePaths.getElementsByTagName("path-element")
+
+            for cPath in cPathsList:
                 self.aSourceDirs.append(cPath.firstChild.data)
 
         # need to check the different ways of inluding sources and amend this as appropriate
-        cSourceSwcs     = self.cConfigXml.getElementsByTagName("include-libraries")
+        cSourceSwcs         = self.cConfigXml.getElementsByTagName("include-libraries")
 
         if cSourceSwcs != None:
-            cSourceSwcs     = cSourceSwcs[0].getElementsByTagName("library")
+            cSourceSwcs     = cSourceSwcs[0]
+            cLibraryList    = cSourceSwcs.getElementsByTagName("library")
 
-            for cLibrary in cSourceSwcs:
+            for cLibrary in cLibraryList:
                 self.aSourceSwcs.append(cLibrary.firstChild.data)
 
         aAbsPaths   = []
@@ -79,6 +99,5 @@ class FlexConfigParser:
     def getSourceSwcs(self):
         return self.aSourceSwcs
 
-	# TODO: implement this :o
     def getAppendSourceFlag(self):
-        return True
+        return self.bAppendSrcPath
